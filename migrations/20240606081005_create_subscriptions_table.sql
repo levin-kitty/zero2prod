93209e0c3cd8b52ca75c386d19b1dfaf690fdda0
levin.kitty <levin.de.kitty@gmail.com>
2024-06-11 21:50:31 +0900

    ~ chapter 3

---
+++ migrations/20240606081005_create_subscriptions_table.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+CREATE TABLE subscriptions
+(
+    id            uuid        NOT NULL,
+    email         TEXT        NOT NULL UNIQUE,
+    name          TEXT        NOT NULL,
+    subscribed_at timestamptz NOT NULL,
+    PRIMARY KEY (id)
+)
